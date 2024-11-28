de9091d4229f21f5651dcc588686f25dd4b21c1b
lyqluis <woyaozhucelushi@163.com>
2024-11-28 13:27:37 +0800

    change telescope key binding

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,12 @@
 return {
-  'nvim-telescope/telescope.nvim', 
-  tag = '0.1.8',  -- or, branch = '0.1.x',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  cmd = 'Telescope',  -- lazy load on cmd
-  keys = {
-    { "<leader>p", ":Telescope find_files<cr>", desc = "find files (@Telescope)" },
-    -- need to install ripgrep
-    { "<leader>P", ":Telescope live_grep<cr>", desc = "find files by regexp (@Telescope)" },
-    { "<leader>of", ":Telescope oldfiles<cr>", desc = "find recent files (@Telescope)" },
-    { "<leader>rs", ":Telescope resume<cr>", desc = "restore last search list (@Telescope)" },
-  },
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.8", -- or, branch = '0.1.x',
+	dependencies = { "nvim-lua/plenary.nvim" },
+	cmd = "Telescope", -- lazy load on cmd
+	keys = {
+		{ "<D-p>", ":Telescope find_files<cr>", desc = "find files (@Telescope)" },
+		{ "<D-P>", ":Telescope live_grep<cr>", desc = "find files by regexp (@Telescope)" }, -- need to install ripgrep
+		{ "<leader>of", ":Telescope oldfiles<cr>", desc = "find recent files (@Telescope)" },
+		{ "<leader>rs", ":Telescope resume<cr>", desc = "restore last search list (@Telescope)" },
+	},
 }
-
