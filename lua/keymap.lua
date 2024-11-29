0f9b044d1a87ebdf43f3d731ab3a1ee18a546cb2
lyqluis <woyaozhucelushi@163.com>
2024-11-29 13:21:36 +0800

    config: add key binding for switching between built-in windows

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,2 +1,12 @@
-local map = vim.api.nvim_set_keymap
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- switch between windows
+map("n", "<leader>h", "<C-w>h", opts)
+map("n", "<leader>l", "<C-w>l", opts)
+map("n", "<leader>j", "<C-w>j", opts)
+map("n", "<leader>k", "<C-w>k", opts)
+map("n", "<leader><Tab>", "<C-w>w", opts)
