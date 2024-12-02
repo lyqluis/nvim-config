168f5704c58e350bbaf726b640e5805d24347107
lyqluis <woyaozhucelushi@163.com>
2024-12-02 15:48:54 +0800

    config: code folding highlight

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,29 @@
 return {
-  {
+	{
 		"norcalli/nvim-colorizer.lua",
 		config = function()
 			require("colorizer").setup()
 		end,
 	},
-  {
-    "olimorris/onedarkpro.nvim",
-    priority = 1000, -- Ensure it loads first
-  },
+	{
+		"olimorris/onedarkpro.nvim",
+		priority = 1000, -- Ensure it loads first
+		config = function()
+			require("onedarkpro").setup({
+				highlights = {
+					Folded = {
+						-- bg = "#5c6370",
+						bg = "#394863",
+					},
+				},
+			})
+		end,
+	},
+	--   {
+	--     'nvim-lualine/lualine.nvim',
+	--     config = function()
+	--         require('lualine').setup {
+	--         }
+	--     end
+	-- },
 }
--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -1,4 +1,32 @@
 -- code folding
+local fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
+	local newVirtText = {}
+	local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+	local sufWidth = vim.fn.strdisplaywidth(suffix)
+	local targetWidth = width - sufWidth
+	local curWidth = 0
+	for _, chunk in ipairs(virtText) do
+		local chunkText = chunk[1]
+		local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+		if targetWidth > curWidth + chunkWidth then
+			table.insert(newVirtText, chunk)
+		else
+			chunkText = truncate(chunkText, targetWidth - curWidth)
+			local hlGroup = chunk[2]
+			table.insert(newVirtText, { chunkText, hlGroup })
+			chunkWidth = vim.fn.strdisplaywidth(chunkText)
+			-- str width returned from truncate() may less than 2nd argument, need padding
+			if curWidth + chunkWidth < targetWidth then
+				suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+			end
+			break
+		end
+		curWidth = curWidth + chunkWidth
+	end
+	table.insert(newVirtText, { suffix, "MoreMsg" })
+	return newVirtText
+end
+
 return {
 	{
 		"kevinhwang91/nvim-ufo",
@@ -19,7 +47,9 @@ return {
 			-- 		-- you can add other fields for setting up lsp server in this table
 			-- 	})
 			-- end
-			require("ufo").setup()
+			require("ufo").setup({
+				fold_virt_text_handler = fold_virt_text_handler,
+			})
 		end,
 	},
 }
