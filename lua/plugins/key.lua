fbd36829ba0f1e7211185e7ff3628f9b83332650
lyqluis <woyaozhucelushi@163.com>
2024-12-05 13:07:50 +0800

    plugin: kylechui/nvim-surround, add operator for quick manipulation of pair surround

--- lua/plugins/key.lua
+++ lua/plugins/key.lua
@@ -24,4 +24,15 @@ return {
 			},
 		},
 	},
+	{
+		-- add operator to add/delete/change surrounding pairs quickly
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
 }
