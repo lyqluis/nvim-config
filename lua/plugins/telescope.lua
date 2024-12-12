e78f8b6c2b1922c54fb583009281f99b9b394c07
lyqluis <woyaozhucelushi@163.com>
2024-12-12 12:40:17 +0800

    change key map

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,8 +8,11 @@ return {
 		},
 		cmd = "Telescope", -- lazy load on cmd
 		keys = {
-			{ "<D-p>", ":Telescope find_files<cr>", desc = "find files (@Telescope)" },
-			{ "<D-P>", ":Telescope live_grep<cr>", desc = "find files by regexp (@Telescope)" }, -- need to install ripgrep
+			{ "<leader>p", ":Telescope find_files<cr>", desc = "find files (@Telescope)" },
+			{ "<leader>P", ":Telescope live_grep<cr>", desc = "find files by regexp (@Telescope)" }, -- need to install ripgrep
+
+			-- { "<D-p>", ":Telescope find_files<cr>", desc = "find files (@Telescope)" },
+			-- { "<D-P>", ":Telescope live_grep<cr>", desc = "find files by regexp (@Telescope)" }, -- need to install ripgrep
 			{ "<leader>of", ":Telescope oldfiles<cr>", desc = "find recent files (@Telescope)" },
 			{ "<leader>rs", ":Telescope resume<cr>", desc = "restore last search list (@Telescope)" },
 		},
