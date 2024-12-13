16c09e3b05b2d135d91b4f3325a0d06fe645a97a
lyqluis <woyaozhucelushi@163.com>
2024-12-13 14:29:04 +0800

    plugin: conform.nvim, set custom command `Format` to replace lsp default format

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -9,120 +9,96 @@ require("mason-lspconfig").setup()
 
 -- fallback if lsp exit suddenly
 vim.api.nvim_create_autocmd("LspDetach", {
-  callback = function(args)
-    -- print("detaching LSP..")
-    -- local bufname = vim.fn.bufname(args.buf)
-    -- if bufname:match("%.go$") then
-    -- print("bufname " .. bufname)
-    -- print("restarting LSP..")
-    vim.cmd("LspStart")
-    -- end
-  end,
+	callback = function(args)
+		-- print("detaching LSP..") local bufname = vim.fn.bufname(args.buf) if
+		-- bufname:match("%.go$") then print("bufname " .. bufname) print("restarting
+		-- LSP..")
+		vim.cmd("LspStart")
+		-- end
+	end,
 })
 
-local lsp_formatting = function(bufnr)
-  vim.lsp.buf.format({
-    filter = function(client)
-      -- apply whatever logic you want (in this example, we'll only use null-ls)
-      return client.name == "null-ls"
-    end,
-    bufnr = bufnr,
-    -- timeout_ms = 2000,
-    -- async = true,
-  })
-end
 -- if you want to set up formatting on save, you can use this as a callback
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+-- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local on_attach = function(client, bufnr)
-  vim.cmd("syntax on")
-  -- if client.name == "gopls" and not client.server_capabilities.semanticTokensProvider then
-  -- 	local semantic = client.config.capabilities.textDocument.semanticTokens
-  -- 	client.server_capabilities.semanticTokensProvider = {
-  -- 		full = true,
-  -- 		legend = { tokenModifiers = semantic.tokenModifiers, tokenTypes = semantic.tokenTypes },
-  -- 		range = true,
-  -- 	}
-  -- end
-  local function buf_set_keymap(...)
-    vim.api.nvim_buf_set_keymap(bufnr, ...)
-  end
+	vim.cmd("syntax on")
+	-- if client.name == "gopls" and not
+	-- client.server_capabilities.semanticTokensProvider then local semantic =
+	-- client.config.capabilities.textDocument.semanticTokens
+	-- client.server_capabilities.semanticTokensProvider = { full = true,
+	-- legend = { tokenModifiers = semantic.tokenModifiers, tokenTypes =
+	-- semantic.tokenTypes }, range = true,
+	-- 	} end
+	local function buf_set_keymap(...)
+		vim.api.nvim_buf_set_keymap(bufnr, ...)
+	end
 
-  local function buf_set_option(...)
-    vim.api.nvim_buf_set_option(bufnr, ...)
-  end
+	local function buf_set_option(...)
+		vim.api.nvim_buf_set_option(bufnr, ...)
+	end
 
-  -- add to your shared on_attach callback
-  if client.supports_method("textDocument/formatting") then
-    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      group = augroup,
-      buffer = bufnr,
-      callback = function()
-        lsp_formatting(bufnr)
-      end,
-    })
-  end
+	-- add to your shared on_attach callback
 
-  -- buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+	-- buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
-  -- cousor hold for 3 seconds, show signature helper
-  -- silent
-  -- vim.api.nvim_command([[autocmd CursorHold  <buffer> lua vim.lsp.buf.hover() ]])
+	-- cousor hold for 3 seconds, show signature helper silent
+	-- vim.api.nvim_command([[autocmd CursorHold  <buffer> lua
+	-- vim.lsp.buf.hover() ]])
 
-  -- -- Mappings.
-  -- defult mapping:
-  -- <C-]>, go to definition
-  -- Enables (manual) omni mode completion with <C-X><C-O> in Insert mode. For autocompletion, an autocompletion plugin is required.
-  -- Enables LSP formatting with gq.
-  -- [d, vim.diagnostic.goto_prev()
-  -- ]d, vim.diagnostic.goto_next()
-  -- <C-W>d, vim.diagnostic.open_float()
-  -- gd, vim.lsp.buf.definition
-  -- gD, vim.lsp.buf.declaration
-  -- K, vim.lsp.buf.hover
-  local opts = { noremap = true, silent = true }
-  buf_set_keymap("n", "Ï", ":lua vim.lsp.buf.format()<CR>", opts) -- format: shift + alt + f
+	-- -- Mappings.
+	-- defult mapping:
+	-- <C-]>, go to definition
+	-- Enables (manual) omni mode completion with <C-X><C-O> in Insert mode. For autocompletion, an autocompletion plugin is required.
+	-- Enables LSP formatting with gq.
+	-- [d, vim.diagnostic.goto_prev()
+	-- ]d, vim.diagnostic.goto_next()
+	-- <C-W>d, vim.diagnostic.open_float()
+	-- gd, vim.lsp.buf.definition
+	-- gD, vim.lsp.buf.declaration
+	-- K, vim.lsp.buf.hover
+	local opts = { noremap = true, silent = true }
+	buf_set_keymap("n", "Ï", "<cmd>Format<CR>", opts) -- format: shift + alt + f
 
-  -- buf_set_keymap("n", "<Enter>", "<Nop>", opts)
-  -- buf_set_keymap("n", "gD", "<cmd>Lspsaga goto_type_definition<CR>", opts)
-  -- buf_set_keymap("n", "gd", "<cmd>Lspsaga goto_definition<CR>", opts)
-  -- -- buf_set_keymap("n", "gv", "<cmd>Lspsaga peek_definition<CR>", opts)
-  -- buf_set_keymap("n", "<Leader>ga", "<cmd>Lspsaga code_action<CR>", opts)
-  -- -- -- coode action for extract function or variable
-  -- -- buf_set_keymap("v", "ga", "cmd>lua vim.lsp.bug.code_action()<CR>", opts)
-  buf_set_keymap("n", "<leader>ca", ":lua vim.lsp.buf.code_action()<CR>", opts)           -- 对光标位置代码进行操作
-  buf_set_keymap("v", "<leader>ca", "<cmd>lua vim.lsp.buf.range_code_action()<CR>", opts) -- 对代码块进行操作
-  -- buf_set_keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
-  -- buf_set_keymap("n", "<space>dt", "<cmd>lua require('dap-go').debug_test()<CR>", opts)
-  -- buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-  -- buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-  -- buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-  -- buf_set_keymap("n", "<space>rn", "<cmd>Lspsaga rename<CR>", opts)
-  -- -- buf_set_keymap("n", "<space>gr", "<cmd>Lspsaga finder<CR>", opts)
-  -- vim.keymap.set("n", "<space>gr", vim.lsp.buf.references, opts)
-  -- vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-  -- buf_set_keymap("n", "<S-A>f", ":lua vim.lsp.buf.format()<CR>", opts)
-  -- -- if current buff end with _test.go, then set keymap for error
-  -- local buf_name = vim.api.nvim_buf_get_name(bufnr)
-  -- buf_set_keymap("n", "<space>ge", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
-  --
-  -- -- Set autocommands conditional on server_capabilities
-  -- if client.server_capabilities.document_highlight then
-  -- 	vim.api.nvim_exec(
-  -- 		[[
-  --      hi LspReferenceRead cterm=bold ctermbg=DarkMagenta guibg=LightYellow
-  --      hi LspReferenceText cterm=bold ctermbg=DarkMagenta guibg=LightYellow
-  --      hi LspReferenceWrite cterm=bold ctermbg=DarkMagenta guibg=LightYellow
-  --      augroup lsp_document_highlight
-  --        autocmd! * <buffer>
-  --        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-  --        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-  --      augroup END
-  --    ]],
-  -- 		false
-  -- 	)
-  -- end
+	-- buf_set_keymap("n", "<Enter>", "<Nop>", opts)
+	-- buf_set_keymap("n", "gD", "<cmd>Lspsaga goto_type_definition<CR>", opts)
+	-- buf_set_keymap("n", "gd", "<cmd>Lspsaga goto_definition<CR>", opts)
+	-- -- buf_set_keymap("n", "gv", "<cmd>Lspsaga peek_definition<CR>", opts)
+	-- buf_set_keymap("n", "<Leader>ga", "<cmd>Lspsaga code_action<CR>", opts)
+	-- -- -- coode action for extract function or variable
+	-- -- buf_set_keymap("v", "ga", "cmd>lua vim.lsp.bug.code_action()<CR>", opts)
+	buf_set_keymap("n", "<leader>ca", ":lua vim.lsp.buf.code_action()<CR>", opts) -- 对光标位置代码进行操作
+	buf_set_keymap("v", "<leader>ca", "<cmd>lua vim.lsp.buf.range_code_action()<CR>", opts) -- 对代码块进行操作
+	-- buf_set_keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
+	-- buf_set_keymap("n", "<space>dt", "<cmd>lua require('dap-go').debug_test()<CR>", opts)
+	-- buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+	-- buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+	-- buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+	-- buf_set_keymap("n", "<space>rn", "<cmd>Lspsaga rename<CR>", opts)
+	-- -- buf_set_keymap("n", "<space>gr", "<cmd>Lspsaga finder<CR>", opts)
+	-- vim.keymap.set("n", "<space>gr", vim.lsp.buf.references, opts)
+	-- vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+	-- buf_set_keymap("n", "<S-A>f", ":lua vim.lsp.buf.format()<CR>", opts)
+	-- -- if current buff end with _test.go, then set keymap for error
+	-- local buf_name = vim.api.nvim_buf_get_name(bufnr)
+	-- buf_set_keymap("n", "<space>ge", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
+	--
+	-- -- Set autocommands conditional on server_capabilities
+	-- if client.server_capabilities.document_highlight then
+	-- 	vim.api.nvim_exec(
+	-- 		[[
+	--      hi LspReferenceRead cterm=bold ctermbg=DarkMagenta guibg=LightYellow
+	--      hi LspReferenceText cterm=bold ctermbg=DarkMagenta guibg=LightYellow
+	--      hi LspReferenceWrite cterm=bold ctermbg=DarkMagenta guibg=LightYellow
+	--      augroup lsp_document_highlight
+	--        autocmd! * <buffer>
+	--        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+	--        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+	--      augroup END
+	--    ]],
+	-- 		false
+	-- 	)
+	-- end
 end
 
 -- Set up lspconfig.
@@ -143,28 +119,27 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 -- -- code folding
 -- local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.foldingRange = {
-  dynamicRegistration = false,
-  lineFoldingOnly = true,
+	dynamicRegistration = false,
+	lineFoldingOnly = true,
 }
 
 local language_servers = {
-  "lua_ls",
-  -- "ts_ls"
+	"lua_ls",
+	-- "ts_ls"
 }
 -- local language_servers = lspconfig.util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
 for _, ls in ipairs(language_servers) do
-  lspconfig[ls].setup({
-    capabilities = capabilities,
-    -- you can add other fields for setting up lsp server in this table
-    on_attach = on_attach,
-  })
+	lspconfig[ls].setup({
+		capabilities = capabilities,
+		-- you can add other fields for setting up lsp server in this table
+		on_attach = on_attach,
+	})
 end
 
-
 -- typscript-tools
-require("typescript-tools").setup {
-  capabilities = capabilities,
-  on_attach = on_attach,
-  handlers = {},
-  settings = {},
-}
+require("typescript-tools").setup({
+	capabilities = capabilities,
+	on_attach = on_attach,
+	handlers = {},
+	settings = {},
+})
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,83 +1,83 @@
 return {
-  -- {
-  -- 	"nvimtools/none-ls.nvim",
-  -- 	dependencies = {
-  -- 		"nvimtools/none-ls-extras.nvim", -- require other no-builtin resources
-  -- 	},
-  -- 	event = "VeryLazy",
-  -- 	config = function()
-  -- 		-- setup null-ls
-  -- 		local null_ls = require("null-ls")
-  -- 		null_ls.setup({
-  -- 			sources = {
-  -- 				-- sources need to be installed in Mason
-  -- 				-- lua,luau
-  -- 				null_ls.builtins.formatting.stylua,
-  -- 				-- js,ts
-  -- 				require("none-ls.diagnostics.eslint_d"),
-  -- 				null_ls.builtins.formatting.prettier,
-  -- 				-- sh
-  -- 				null_ls.builtins.formatting.shfmt,
-  -- 				-- nginx
-  -- 				-- null_ls.builtins.formatting.nginx_beautifier,
-  -- 				-- null_ls.builtins.code_actions.shellcheck,
-  -- 				-- for python
-  -- 				-- null_ls.builtins.formatting.black,
-  -- 				-- null_ls.builtins.formatting.isort,
-  -- 				-- null_ls.builtins.diagnostics.hadolint,
-  -- 				-- null_ls.builtins.formatting.nixfmt,
-  -- 			},
-  -- 			debug = true,
-  -- 		})
-  -- 	end,
-  -- },
+	-- {
+	-- 	"nvimtools/none-ls.nvim",
+	-- 	dependencies = {
+	-- 		"nvimtools/none-ls-extras.nvim", -- require other no-builtin resources
+	-- 	},
+	-- 	event = "VeryLazy",
+	-- 	config = function()
+	-- 		-- setup null-ls
+	-- 		local null_ls = require("null-ls")
+	-- 		null_ls.setup({
+	-- 			sources = {
+	-- 				-- sources need to be installed in Mason
+	-- 				-- lua,luau
+	-- 				null_ls.builtins.formatting.stylua,
+	-- 				-- js,ts
+	-- 				require("none-ls.diagnostics.eslint_d"),
+	-- 				null_ls.builtins.formatting.prettier,
+	-- 				-- sh
+	-- 				null_ls.builtins.formatting.shfmt,
+	-- 				-- nginx
+	-- 				-- null_ls.builtins.formatting.nginx_beautifier,
+	-- 				-- null_ls.builtins.code_actions.shellcheck,
+	-- 				-- for python
+	-- 				-- null_ls.builtins.formatting.black,
+	-- 				-- null_ls.builtins.formatting.isort,
+	-- 				-- null_ls.builtins.diagnostics.hadolint,
+	-- 				-- null_ls.builtins.formatting.nixfmt,
+	-- 			},
+	-- 			debug = true,
+	-- 		})
+	-- 	end,
+	-- },
 
-  -- ts-server 替代品
-  {
-    "pmizio/typescript-tools.nvim",
-    dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-    event = "VeryLazy",
-    config = function()
-      local api = require 'typescript-tools.api'
-      require('typescript-tools').setup {
-        handlers = {
-          -- filter Error
-          ['textDocument/publishDiagnostics'] = api.filter_diagnostics { 6133 }, -- [tsserver 6133] [I] 'React' is declared but its value is never read
-        },
-        settings = {
-          -- https://github.com/microsoft/TypeScript/blob/v5.0.4/src/server/protocol.ts
-          tsserver_file_preferences = {
-            -- importModuleSpecifierPreference = 'non-relative',
-          },
-        },
-      }
-      local autocmd = vim.api.nvim_create_autocmd
-      -- :w 的时候自动导入未导入模组以及对import排序
-      autocmd('BufWritePre', {
-        pattern = '*.ts,*.tsx,*.jsx,*.js',
-        callback = function(args)
-          vim.cmd 'TSToolsAddMissingImports sync'
-          vim.cmd 'TSToolsOrganizeImports sync'
-          -- Todo: require('conform').format { bufnr = args.buf }
-        end,
-      })
-    end,
-  },
-  -- https://github.com/williamboman/mason.nvim
-  {
-    "williamboman/mason.nvim",
-    -- event = 'VeryLazy',
-    cmd = { "Mason", "MasonInstall", "MasonUpdate" },
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    event = "VeryLazy",
-  },
-  {
-    "neovim/nvim-lspconfig",
-    event = "VeryLazy",
-  },
+	-- ts-server 替代品
+	{
+		"pmizio/typescript-tools.nvim",
+		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+		event = "VeryLazy",
+		config = function()
+			local api = require("typescript-tools.api")
+			require("typescript-tools").setup({
+				handlers = {
+					-- filter Error
+					["textDocument/publishDiagnostics"] = api.filter_diagnostics({ 6133 }), -- [tsserver 6133] [I] 'React' is declared but its value is never read
+				},
+				settings = {
+					-- https://github.com/microsoft/TypeScript/blob/v5.0.4/src/server/protocol.ts
+					tsserver_file_preferences = {
+						-- importModuleSpecifierPreference = 'non-relative',
+					},
+				},
+			})
+			-- :w 的时候进行操作：自动导入缺失模块，排序import，format
+			local autocmd = vim.api.nvim_create_autocmd
+			autocmd("BufWritePre", {
+				pattern = "*.ts,*.tsx,*.jsx,*.js",
+				callback = function(args)
+					vim.cmd("TSToolsAddMissingImports sync")
+					vim.cmd("TSToolsOrganizeImports sync")
+					require("conform").format({ bufnr = args.buf })
+				end,
+			})
+		end,
+	},
+	-- https://github.com/williamboman/mason.nvim
+	{
+		"williamboman/mason.nvim",
+		-- event = 'VeryLazy',
+		cmd = { "Mason", "MasonInstall", "MasonUpdate" },
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		event = "VeryLazy",
+	},
+	{
+		"neovim/nvim-lspconfig",
+		event = "VeryLazy",
+	},
 }
