345211bd17fc77deaabea7a7cf17f470ef04a73b
lyqluis <woyaozhucelushi@163.com>
2024-12-16 13:55:38 +0800

    plugin: nvim-ts-autotag, rename input related plugins file

--- lua/plugins/input.lua
+++ lua/plugins/input.lua
@@ -1,5 +1,19 @@
 return {
 	{
+		"windwp/nvim-ts-autotag",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("nvim-ts-autotag").setup({
+				opts = {
+					-- Defaults
+					enable_close = true, -- Auto close tags
+					enable_rename = true, -- Auto rename pairs of tags
+					enable_close_on_slash = false, -- Auto close on trailing </
+				},
+			})
+		end,
+	},
+	{
 		"windwp/nvim-autopairs",
 		event = "InsertEnter",
 		config = true,
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,20 @@ return {
 			local configs = require("nvim-treesitter.configs")
 			configs.setup({
 				-- A list of parser names, or "all" (the listed parsers MUST always be installed)
-				ensure_installed = { "lua", "vim", "markdown", "javascript", "typescript" },
+				ensure_installed = {
+					"lua",
+					"vim",
+					"javascript",
+					"typescript",
+					"html",
+					"css",
+					"json",
+					"regex",
+					-- "rust",
+					"prisma",
+					"markdown",
+					"markdown_inline",
+				},
 
 				-- Install parsers synchronously (only applied to `ensure_installed`)
 				sync_install = false,
