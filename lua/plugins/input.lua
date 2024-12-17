05fb6be24d1b8b2c23979548ab23855ee5c25cbc
lyqluis <woyaozhucelushi@163.com>
2024-12-17 16:15:43 +0800

    plugin: nvim-cursorline

--- lua/plugins/input.lua
+++ lua/plugins/input.lua
@@ -57,4 +57,22 @@ return {
 			})
 		end,
 	},
+	{
+		"yamatsum/nvim-cursorline",
+		-- event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("nvim-cursorline").setup({
+				-- cursorline = {
+				-- 	enable = true,
+				-- 	timeout = 1000,
+				-- 	number = false,
+				-- },
+				cursorword = {
+					enable = true,
+					min_length = 3,
+					hl = { underline = true },
+				},
+			})
+		end,
+	},
 }
